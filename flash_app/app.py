91e75c6ef980b7a46ee6a0d9310918e066d3f142
Revi <oliiv@outlook.com>
2022-08-11 12:25:38 +0200

    use root as route

--- flash_app/app.py
+++ flash_app/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 api = Api(app)
 
 
-class Users(Resource):
+class Root(Resource):
     def get(self):
         parser = reqparse.RequestParser()  # initialize
         parser.add_argument('option', required=True)
@@ -14,7 +14,7 @@ class Users(Resource):
         return {'result': result}, 200  # return data and 200 OK
 
 
-api.add_resource(Users, '/users')  # add endpoints
+api.add_resource(Root, '/')  # add endpoints
 
-# if __name__ == '__main__':
-#   app.run()  # run our Flask app
+if __name__ == '__main__':
+    app.run()  # run our Flask app
